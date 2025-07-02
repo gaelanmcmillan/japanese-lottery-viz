ca20b58799fd3ee72b48123ab55258a9b9da4743
gaelanmcmillan <gaelan.mcmillan@gmail.com>
2025-07-01 22:32:02 -0400

    delete everything except index and bundle

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import * as P5 from "p5";
-import { sketch } from "./sketch";
-const _instance = new P5(sketch);
--- src/lib.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./lib";
-
-describe("testing the lib", () => {
-  test("add should add", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
